76470dd98c5ed0ce5552ef3984d6a94069dd28b6
andreaalf97 <andrea.alfieri97@gmail.com>
2019-04-10 16:55:57 +0200

    making tests work again + new UML

--- src/test/java/it/polimi/ingsw/model/AmmoSpotTest.java
+++ src/test/java/it/polimi/ingsw/model/AmmoSpotTest.java
@@ -10,7 +10,7 @@ public class AmmoSpotTest {
 
     @Test
     public void addAmmoWhenSpotIsUnloaded() {
-        AmmoSpot ammoSpotTest = new AmmoSpot(Room.TOPAZ,0,0,0);
+        AmmoSpot ammoSpotTest = new AmmoSpot(Room.TOPAZ,0,0,0, null);
 
         ammoSpotTest.addAmmos();
         Assert.assertFalse(ammoSpotTest.getAmmoColorList().isEmpty());
@@ -18,7 +18,7 @@ public class AmmoSpotTest {
 
     @Test
     public void addAmmoWhenSpotIsAlreadyLoadedWithPowerup(){
-        AmmoSpot ammoSpotTest = new AmmoSpot(Room.SAPPHIRE,0,0,0);
+        AmmoSpot ammoSpotTest = new AmmoSpot(Room.SAPPHIRE,0,0,0, null);
         ArrayList<Color> ammoColorListTest = new ArrayList<>();
 
         ammoColorListTest.add(Color.RED);
@@ -31,7 +31,7 @@ public class AmmoSpotTest {
 
     @Test
     public void addAmmoWhenSpotIsAlreadyLoaded(){
-        AmmoSpot ammoSpotTest = new AmmoSpot(Room.SAPPHIRE,0,0,0);
+        AmmoSpot ammoSpotTest = new AmmoSpot(Room.SAPPHIRE,0,0,0, null);
         ArrayList<Color> ammoColoListTest = new ArrayList<>();
 
         ammoColoListTest.add(Color.RED);
@@ -46,7 +46,7 @@ public class AmmoSpotTest {
 
     @Test
     public  void removeAmmoTrue() {
-        AmmoSpot ammoSpotTest = new AmmoSpot(Room.SAPPHIRE,0,0,0);
+        AmmoSpot ammoSpotTest = new AmmoSpot(Room.SAPPHIRE,0,0,0, null);
         ArrayList<Color> ammoColorListTest = new ArrayList<>();
 
         ammoColorListTest.add(Color.RED);
@@ -58,7 +58,7 @@ public class AmmoSpotTest {
 
     @Test
     public void removeAmmoFalse(){
-        AmmoSpot ammoSpotTest = new AmmoSpot(Room.TOPAZ,0,0,0);
+        AmmoSpot ammoSpotTest = new AmmoSpot(Room.TOPAZ,0,0,0, null);
 
         ammoSpotTest.removeAmmos();
         Assert.assertTrue(ammoSpotTest.getAmmoColorList().isEmpty());
