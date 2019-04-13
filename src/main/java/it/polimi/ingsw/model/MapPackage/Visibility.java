19af507cafc45f2e10672de84d4a6cec88ead7f0
Ame <48249065+meme97@users.noreply.github.com>
2019-04-13 11:45:52 +0200

    Finished json weapons and powerups effects

--- src/main/java/it/polimi/ingsw/model/MapPackage/Visibility.java
+++ src/main/java/it/polimi/ingsw/model/MapPackage/Visibility.java
@@ -2,6 +2,7 @@ package it.polimi.ingsw.model.MapPackage;
 
 public enum Visibility {
     NONE, // 0 in JSON
-    OFFENDER,
-    LASTONEATTACKED
+    DONTCARE,   //1
+    OFFENDER,   //2
+    LASTONEATTACKED //3
 }
