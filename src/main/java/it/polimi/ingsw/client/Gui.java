6bd73e93c3fa0ebbcb540388a8b30f2845e3a52d
Ame <48249065+meme97@users.noreply.github.com>
2019-05-26 17:16:48 +0200

    working on gui

--- src/main/java/it/polimi/ingsw/client/Gui.java
+++ src/main/java/it/polimi/ingsw/client/Gui.java
@@ -3,19 +3,21 @@ package it.polimi.ingsw.client;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Group;
-import javafx.scene.Scene;
+import javafx.scene.*;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.ArcType;
 import javafx.stage.Stage;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
 import javafx.stage.Window;
+import javafx.scene.layout.Pane;
 
 import java.util.ArrayList;
 
@@ -32,26 +34,59 @@ public class Gui extends Application implements UserInterface {
         window.setTitle("Adrenalina");
 
         //Setting up startscene
-        VBox layout1 = new VBox(20);
-        Scene startGameScene = new Scene(layout1, 200, 200);
+        BorderPane StartLayout = new BorderPane();
+        Scene startGameScene = new Scene(StartLayout, 750, 500);
 
         Button startGameButton = new Button("Start game");
         Label label1 = new Label("Welcome to Adrenalina");
         Button closeButton = new Button("Exit");
 
-        closeButton.setOnAction(e -> window.close());
+        closeButton.setOnAction(e -> ClosingBox.display( window ));
 
-        label1.setAlignment(Pos.TOP_CENTER);
-        startGameButton.setAlignment(Pos.CENTER);
-        closeButton.setAlignment(Pos.BOTTOM_RIGHT);
+        StartLayout.setTop(label1);
+        StartLayout.setCenter(startGameButton);
+        StartLayout.setBottom(closeButton);
 
-        layout1.getChildren().addAll(label1, startGameButton, closeButton);
 
         //Login scene
-        VBox layout2 = new VBox(20);
-        Scene loginScene = new Scene(layout2, 200, 200);
+        GridPane loginLayout = new GridPane();
+        loginLayout.setPadding( new Insets(20, 20, 20, 20));
 
-        Label loginLabel = new Label("Username:");
+        loginLayout.setVgap(10);
+        loginLayout.setHgap(8);
+
+        //username input
+        Label usernameLabel = new Label("Username:");
+        //set the username label in the top left
+        GridPane.setConstraints( usernameLabel, 1, 1);
+        //insert the username
+        TextField usernameInput = new TextField();
+        GridPane.setConstraints(usernameInput, 2, 1);
+
+        //choosing map input
+        Label choosingMapLabel = new Label("Choose the map you want to play in:");
+        GridPane.setConstraints(choosingMapLabel, 1, 2);
+        //insert the input
+        TextField choosingMapInput = new TextField();
+        choosingMapInput.setPromptText("Fire, Earth, Wind, Water");
+        GridPane.setConstraints(choosingMapInput, 2, 2);
+
+
+
+        //choosing number of skull input
+        Label numberOfSkullsLabel = new Label("Choose the number of skulls you want to play with:");
+        GridPane.setConstraints(numberOfSkullsLabel, 1, 3);
+        //insert the input
+        TextField numberOfSkullsInput = new TextField();
+        numberOfSkullsInput.setPromptText("between 5 and 8");
+        GridPane.setConstraints(numberOfSkullsInput, 2, 3);
+
+        Button loginButton = new Button("Login");
+        GridPane.setConstraints(loginButton, 2, 4);
+
+        loginLayout.getChildren().addAll(usernameLabel, usernameInput, choosingMapLabel, choosingMapInput, numberOfSkullsLabel, numberOfSkullsInput, loginButton);
+
+        Scene loginScene = new Scene(loginLayout, 750, 500);
 
         startGameButton.setOnAction(e -> window.setScene(loginScene));
 
@@ -59,10 +94,15 @@ public class Gui extends Application implements UserInterface {
 
 
 
+        //Board scene (first scene after logging in)
+        VBox boardSceneLayout = new VBox(20);
+        Scene boardScene = new Scene(boardSceneLayout, 750, 500);
+
+        loginButton.setOnAction(e -> window.setScene(boardScene));
+
 
         window.setScene(startGameScene);
         window.show();
-
     }
 
     @Override
