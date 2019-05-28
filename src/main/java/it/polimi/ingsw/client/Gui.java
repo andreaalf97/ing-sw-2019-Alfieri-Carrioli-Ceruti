1e9ffeb2963fe3c6378505f783b5638b6cb7c220
Ame <48249065+meme97@users.noreply.github.com>
2019-05-28 10:53:42 +0200

    working on gui

--- src/main/java/it/polimi/ingsw/client/Gui.java
+++ src/main/java/it/polimi/ingsw/client/Gui.java
@@ -3,6 +3,9 @@ package it.polimi.ingsw.client;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.exception.InvalidChoiceException;
 import it.polimi.ingsw.model.map.MapName;
+import it.polimi.ingsw.server.ServerInterface;
+import it.polimi.ingsw.view.client.RemoteViewRmiImpl;
+import it.polimi.ingsw.view.client.RemoteViewSocket;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
@@ -28,10 +31,22 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
 import java.util.ArrayList;
 import java.util.regex.Pattern;
 
 public class Gui extends Application implements UserInterface {
+
+    final String validUsername = "^[a-zA-Z0-9]*$";
+
+    final String serverAddress = "127.0.0.1";
+
+    final int rmiPort = 5432;
+
+    final int socketPort = 2345;
 
     public static void main(String[] args) {
         launch(args);
@@ -39,15 +54,6 @@ public class Gui extends Application implements UserInterface {
 
     @Override
     public void start(Stage window) throws Exception {
-
-        final String validUsername = "^[a-zA-Z0-9]*$";
-
-        final String serverAddress = "127.0.0.1";
-
-        final int rmiPort = 5432;
-
-        final int socketPort = 2345;
-
 
         //First window
         window.setTitle("Adrenalina");
@@ -85,6 +91,7 @@ public class Gui extends Application implements UserInterface {
         GridPane.setConstraints( usernameLabel, 1, 1);
         //insert the username
         TextField usernameInput = new TextField();
+
         GridPane.setConstraints(usernameInput, 2, 1);
         //choosing map input
         Label choosingMapLabel = new Label("Choose the map you want to play in:");
@@ -110,28 +117,6 @@ public class Gui extends Application implements UserInterface {
 
         startGameButton.setOnAction(e -> window.setScene(loginScene));
 
-
-
-        //Board scene (first scene after logging in)
-        BorderPane boardSceneLayout = new BorderPane();
-
-        Scene boardScene = new Scene(boardSceneLayout, 1000, 700);
-
-        Image mapImage = new Image(new FileInputStream("src/main/resources/Grafica/Mappe/Mappe/Mappa_1.png"));
-
-        ImageView imageView = new ImageView(mapImage);
-
-        imageView.setFitHeight(boardScene.getHeight());
-        imageView.setFitWidth(boardScene.getWidth());
-
-        boardSceneLayout.getChildren().addAll(imageView);
-
-
-        //imageView.fitWidthProperty().bind(window.widthProperty());
-
-        //boardSceneLayout.setCenter(imageView);
-
-
         //Questi sono per settare la grandezza della scena a screen size!
         /*Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
         //set Stage boundaries to visible bounds of the main screen
@@ -155,31 +140,65 @@ public class Gui extends Application implements UserInterface {
         connectionLayout.getChildren().addAll(connectionLabel, HboxConnections);
         Scene connectionScene = new Scene(connectionLayout, 750, 500);
 
+        //Board scene (Shows the map)
+        BorderPane boardSceneLayout = new BorderPane();
+
+        Scene boardScene = new Scene(boardSceneLayout, 1000, 700);
+
         loginButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
 
                 try{
 
-                    Boolean ok = true;
-                    int nSkulls = Integer.parseInt(numberOfSkullsInput.getText());
-                    String username = usernameInput.getText();
-                    MapName votedMap = MapName.valueOf(choosingMapInput.getText());
+                    boolean ok = true;
 
                     //username non valido, mando messaggio e ritorno alla login
-                    if (!Pattern.matches(validUsername, username)) {
+                    if (!Pattern.matches(validUsername, usernameInput.getText())) {
                         TextBox.display("The username can only contain letters and numbers");
                         ok = false;
                     }
-                    if (username.isEmpty()) {
+                    if (usernameInput.getText().isEmpty()) {
                         TextBox.display("Username can't be empty. Please enter a valid one");
                         ok = false;
                     }
-                    if (choosingMapInput.getText().isEmpty() || (votedMap != MapName.FIRE && votedMap != MapName.WATER && votedMap != MapName.WIND && votedMap != MapName.EARTH)) {
+                    if (choosingMapInput.getText().isEmpty() || (MapName.valueOf(choosingMapInput.getText()) != MapName.FIRE && MapName.valueOf(choosingMapInput.getText()) != MapName.WATER && MapName.valueOf(choosingMapInput.getText()) != MapName.WIND && MapName.valueOf(choosingMapInput.getText()) != MapName.EARTH)) {
                         TextBox.display("Map not valid: choose between 'FIRE', 'WIND', 'WATER'. 'EARTH'");
                         ok = false;
+                    }else {
+                        if(MapName.valueOf(choosingMapInput.getText()) == MapName.FIRE ) {
+                            Image mapImage = new Image(new FileInputStream("src/main/resources/Grafica/Mappe/Mappe/Mappa_1.png"));
+                            ImageView imageView = new ImageView(mapImage);
+                            imageView.setFitHeight(boardScene.getHeight());
+                            imageView.setFitWidth(boardScene.getWidth());
+                            boardSceneLayout.getChildren().addAll(imageView);
+
+                        }
+                        if(MapName.valueOf(choosingMapInput.getText()) == MapName.WATER) {
+                            Image mapImage = new Image(new FileInputStream("src/main/resources/Grafica/Mappe/Mappe/Mappa_2.png"));
+                            ImageView imageView = new ImageView(mapImage);
+                            imageView.setFitHeight(boardScene.getHeight());
+                            imageView.setFitWidth(boardScene.getWidth());
+                            boardSceneLayout.getChildren().addAll(imageView);
+
+                        }
+                        if(MapName.valueOf(choosingMapInput.getText()) == MapName.WIND) {
+                            Image mapImage = new Image(new FileInputStream("src/main/resources/Grafica/Mappe/Mappe/Mappa_3.png"));
+                            ImageView imageView = new ImageView(mapImage);
+                            imageView.setFitHeight(boardScene.getHeight());
+                            imageView.setFitWidth(boardScene.getWidth());
+                            boardSceneLayout.getChildren().addAll(imageView);
+
+                        }
+                        if(MapName.valueOf(choosingMapInput.getText()) == MapName.EARTH) {
+                            Image mapImage = new Image(new FileInputStream("src/main/resources/Grafica/Mappe/Mappe/Mappa_4.png"));
+                            ImageView imageView = new ImageView(mapImage);
+                            imageView.setFitHeight(boardScene.getHeight());
+                            imageView.setFitWidth(boardScene.getWidth());
+                            boardSceneLayout.getChildren().addAll(imageView);
+                        }
                     }
-                    if (numberOfSkullsInput.getText().isEmpty() || nSkulls < 5 || nSkulls > 8) {
+                    if (numberOfSkullsInput.getText().isEmpty() || Integer.parseInt(numberOfSkullsInput.getText()) < 5 || Integer.parseInt(numberOfSkullsInput.getText()) > 8) {
                         TextBox.display("Number of skulls not valid");
                         ok = false;
                     }
@@ -196,15 +215,17 @@ public class Gui extends Application implements UserInterface {
         });
 
 
-        /*socketButton.setOnAction( event -> {
-            Cli.startSocketConnection(username, votedMap, nSkulls);
-            beginSocketConnection(username, votedMap, nSkulls);
+
+        socketButton.setOnAction( event -> {
+
+            startSocketConnection(usernameInput.getText(), MapName.valueOf(choosingMapInput.getText()), Integer.parseInt(numberOfSkullsInput.getText()));
             window.setScene(boardScene);
         });
         RMIButton.setOnAction( event -> {
-            Cli.startRmiConnection(username, votedMap, nSkulls);
+
+            startRmiConnection(usernameInput.getText(), MapName.valueOf(choosingMapInput.getText()), Integer.parseInt(numberOfSkullsInput.getText()));
             window.setScene(boardScene);
-        });*/
+        });
 
 
         closeButton.setOnAction(e -> ClosingBox.display(window));
@@ -213,9 +234,71 @@ public class Gui extends Application implements UserInterface {
         window.show();
     }
 
-    /*private void beginSocketConnection( String username, MapName votedMap, int nSkulls){
-        Cli.startSocketConnection(username, votedMap, nSkulls);
-    }*/
+
+    /**
+     * Starts an rmi connection with the server
+     * @param username the username of the player
+     * @param votedMap the voted map
+     * @param nSkulls the voted skulls
+     */
+    private void startRmiConnection(String username, MapName votedMap, int nSkulls) {
+
+        try {
+
+            //Creates the remote view object to send to the server for callbacks
+            RemoteViewRmiImpl remoteView = new RemoteViewRmiImpl(this);
+
+            //Searches for the registry
+            Registry registry = LocateRegistry.getRegistry(serverAddress, rmiPort);
+
+            //Looks for the 'server' object (only has one method called connect())
+            ServerInterface server = (ServerInterface)registry.lookup("server");
+
+            //Sends the connection message (USERNAME:VOTEDMAP:VOTEDSKULLS)
+            String connectionMessage = username + ":" + votedMap + ":" + nSkulls;
+
+            //Call the remote method CONNECT
+            server.connect(remoteView, connectionMessage);
+
+            return;
+
+
+        }
+        catch (Exception e){
+            TextBox.display("Error while connecting to server");
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * Starts a new socket connection with the server
+     * @param username the username of the player
+     * @param votedMap the voted map
+     * @param nSkulls the voted skulls
+     */
+    private void startSocketConnection(String username, MapName votedMap, int nSkulls) {
+
+        Socket serverSocket = null;
+        try {
+            //Connects with the server through socket
+            serverSocket = new Socket(serverAddress, socketPort);
+        }
+        catch (IOException e){
+            System.err.println(e.getMessage());
+        }
+
+        //Creates a new RemoteViewSocket object which is used to keep the connection open and read all new messages
+        RemoteViewSocket remoteViewSocket = new RemoteViewSocket(serverSocket, this);
+
+        //sends the connection message to the server
+        remoteViewSocket.sendMessage(username + ":" + votedMap + ":" + nSkulls);
+
+        //runs the remoteViewSocket on a new thread to keep it open
+        new Thread(remoteViewSocket).run();
+
+    }
 
     @Override
     public void notify(String json) {
