348dec3ae93e562f14486fe42dcbaaa16997a3ef
Andrea <44469014+andreaceruti@users.noreply.github.com>
2019-06-19 15:19:18 +0200

    notify in game created, show map event created

--- src/main/java/it/polimi/ingsw/client/QuestionEventHandler.java
+++ src/main/java/it/polimi/ingsw/client/QuestionEventHandler.java
@@ -32,6 +32,6 @@ public interface QuestionEventHandler {
     void handleEvent(ModelUpdate event);
     void handleEvent(TextMessage event);
     void handleEvent(WhereToMoveAndGrabQuestion event);
-
+    void handleEvent(ShowMapToClientQuestion event);
     void handleEvent(WhereToMoveQuestion event);
 }
