80fe2355e2e3aa1c0641c5916417d639ba529a76
andreaalf97 <andrea.alfieri97@gmail.com>
2019-07-05 11:54:05 +0200

    fixing gui resources

--- src/main/java/it/polimi/ingsw/client/gui/scenes/MyScene.java
+++ src/main/java/it/polimi/ingsw/client/gui/scenes/MyScene.java
@@ -1,10 +1,6 @@
 package it.polimi.ingsw.client.gui.scenes;
 
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 
 public interface MyScene {
 
